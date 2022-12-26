d50e93eb06e260a4080cfa110e880654592c09a4
Makarov <moeyva@yandex.ru>
2022-12-26 23:56:21 +0300

    qw1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        byte a = 1;
+        System.out.println("Значение переменной " + "a" + " с типом " + "byte" + " равно " + a);
+        short b = 2;
+        System.out.println("Значение переменной " + "b" + " с типом " + "short" + " равно " + b);
+        int c = 3;
+        System.out.println("Значение переменной " + "c" + " с типом " + "int" + " равно " + c);
+        long d = 4;
+        System.out.println("Значение переменной " + "d" + " с типом " + "long" + " равно " + d);
+        float g = 10.6f;
+        System.out.println("Значение переменной " + "g" + " с типом " + "float" + " равно " + g);
+        double h = 20.5;
+        System.out.println("Значение переменной " + "h" + " с типом " + "double" + " равно " + h);
     }
 }
